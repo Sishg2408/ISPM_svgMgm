458e79000a3166ad85dfe9b0567a47c6a549f9b0
debasish gadnayak <P000592>
2023-06-09 09:52:49 +0000

    new

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,6 +1,12 @@
 jQuery.sap.declare("com.svg.cwispm.model.formatter");
 com.svg.cwispm.model.formatter= {
-    originalMaterial:function(price,quantity){
+    newTotMaterial:function(price=0,quantity=0){
         return parseInt(price)*parseInt(quantity);
+    },
+    originalMat:function(price=0,quantity=0){
+        return parseInt(price)*parseInt(quantity);
+    },
+    totMat:function(priceN=0,quantityN=0,priceO=0,quantityO=0){
+        return (parseInt(priceO)*parseInt(quantityO))-(parseInt(priceN)*parseInt(quantityN));
     }
 }
